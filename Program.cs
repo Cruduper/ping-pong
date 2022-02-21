3063d38209b1c18308c240a66c4db1d9967c615c
Eric Crudup <briefcasemanx@gmail.com>
2022-02-21 14:42:23 -0800

    finished for loop
    Co-authored-by: Jase Seeley <jcseeley@users.noreply.github.com>

--- Program.cs
+++ Program.cs
@@ -7,7 +7,22 @@ class Program
   static void Main()
   {
 
-    Console.WriteLine()
+    Console.WriteLine("please type a number: ");
+    string response = Console.ReadLine();
+
+    int yourNumber = int.Parse(response);
+
+    for(int i = 1; i <= yourNumber ; i++){
+      if( (i % 3 == 0) && (i % 5 == 0) ){
+        Console.WriteLine("ping-pong");
+      } else if ( i % 5 == 0 ){
+        Console.WriteLine("pong");
+      } else if ( i % 3 == 0 ){
+        Console.WriteLine("ping");
+      } else {
+        Console.WriteLine( i );
+      }
+    }
     // program code goes here
   }
 }
